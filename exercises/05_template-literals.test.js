2ab585a35edd2749df7a3ea32ba99379c1175b2d
CarloFidel <carloftaboada@gmail.com>
2026-02-03 21:46:52 +0100

    feature: exercice_05

--- exercises/05_template-literals.test.js
+++ exercises/05_template-literals.test.js
@@ -1,47 +1,50 @@
-test('05_template-literals-1: should support string interpolation', () => {
+test("05_template-literals-1: should support string interpolation", () => {
   const person = {
-    name: 'Kent C. Dodds',
+    name: "Kent C. Dodds",
     friends: [
-      'Brooke Dodds',
-      'Matt Zabriskie',
-      'Aaron Frost',
-      'Dave Geddes',
-      'Joe Eames',
-      'Ryan Florence',
+      "Brooke Dodds",
+      "Matt Zabriskie",
+      "Aaron Frost",
+      "Dave Geddes",
+      "Joe Eames",
+      "Ryan Florence",
     ],
-  }
+  };
   // construeix una cadena utilitzant la interpolació de literals de plantilla
-  const personsFriends = ``
+  const personsFriends = `${person.name} has ${person.friends.length} friends: ${person.friends.join(", ")}`;
   expect(personsFriends).toBe(
-    'Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence',
-  )
-})
+    "Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence",
+  );
+});
 
-test('05_template-literals-2: should support multi-line strings', () => {
+test("05_template-literals-2: should support multi-line strings", () => {
   // construeix una cadena amb múltiples línies sense necessitat de caràcters de nova línia escapats
-  const multiLine = ``
-  expect(multiLine).toBe('\n    How cool\n    is this!?\n  ')
-})
 
-test('05_template-literals-3: should support string escaping', () => {
+  const multiLine = `
+    How cool
+    is this!?
+  `;
+  expect(multiLine).toBe("\n    How cool\n    is this!?\n  ");
+});
+
+test("05_template-literals-3: should support string escaping", () => {
   // escapa correctament una cadena en un literal de plantilla per a cadascun d'aquests casos
-  expect(``).toBe('Hi\nthere!')
-  expect(``).toBe('This is `escaped` backticks')
-})
+  expect(`Hi\nthere!`).toBe("Hi\nthere!");
+  expect(`This is \`escaped\` backticks`).toBe("This is `escaped` backticks");
+});
 
 //////// EXTRA CREDIT ////////
 
 // és probable que no utilitzis sovint l'etiquetatge, però pot ser útil!
-test.skip('05_template-literals-4: should call the tagging function', () => {
+test.skip("05_template-literals-4: should call the tagging function", () => {
   const tagIt = (literalString, ...interpolatedParts) => {
     // implementa aquesta funció per fer que el test passi
-    return 'fixme'
-  }
-  const noun = 'World'
-  const emotion = 'happy'
-  const result = tagIt`Hello ${noun}! Are you feeling ${emotion} today?`
+    return "fixme";
+  };
+  const noun = "World";
+  const emotion = "happy";
+  const result = tagIt`Hello ${noun}! Are you feeling ${emotion} today?`;
   expect(result).toBe(
-    'Hello super-cool World! Are you feeling really happy today?',
-  )
-
-})
+    "Hello super-cool World! Are you feeling really happy today?",
+  );
+});
