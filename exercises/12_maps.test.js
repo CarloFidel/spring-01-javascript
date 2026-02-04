6a4766049c3d671ab123280d4e860287c73f9a29
CarloFidel <carloftaboada@gmail.com>
2026-02-04 18:31:15 +0100

    feature 12

--- exercises/12_maps.test.js
+++ exercises/12_maps.test.js
@@ -1,25 +1,27 @@
-test('12_maps-1: has a map method', () => {
+test("12_maps-1: has a map method", () => {
   // Crea un nou mapa anomenat 'myMap'
   // Afegeix una nova entrada. Utilitza 'name' com a clau i 'Aaron' com a valor
+  const myMap = new Map();
+  myMap.set('name', 'Aaron');
+  
+  expect(myMap.get("name")).toBe("Aaron");
+});
 
-  expect(myMap.get('name')).toBe('Aaron')
-})
-
-test('12_maps-2: can use objects as a key', () => {
-  const user = {name: 'Aaron'}
-  const value = {twitter: '@js_dev', gplus: '+AaronFrost'}
+test("12_maps-2: can use objects as a key", () => {
+  const user = { name: "Aaron" };
+  const value = { twitter: "@js_dev", gplus: "+AaronFrost" };
 
   // Crea un mapa anomenat 'myMap'
   // Afegeix una nova entrada. Utilitza user com a clau i value com a valor
 
-  expect(myMap.has(user)).toBe(true)
-  expect(myMap.get(user)).toBe(value)
-})
+  expect(myMap.has(user)).toBe(true);
+  expect(myMap.get(user)).toBe(value);
+});
 
 test(`12_maps-3: doesn't coerce keys`, () => {
-  const myMap = new Map()
-  myMap.set(1, 'Aaron')
-  expect(myMap.get('1')).toBe(/*INTRODUEIX LA TEVA RESPOSTA AQUÍ*/)
-  myMap.set('1', 'Aaron')
-  expect(myMap.get('1')).toBe(/*INTRODUEIX LA TEVA RESPOSTA AQUÍ*/)
-})
+  const myMap = new Map();
+  myMap.set(1, "Aaron");
+  expect(myMap.get("1")).toBe(/*INTRODUEIX LA TEVA RESPOSTA AQUÍ*/);
+  myMap.set("1", "Aaron");
+  expect(myMap.get("1")).toBe(/*INTRODUEIX LA TEVA RESPOSTA AQUÍ*/);
+});
